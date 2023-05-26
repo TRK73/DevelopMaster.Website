d2e8ea082b9af2ca4acb4d64dcf0107b0627d8c3
DevelopMaster <127537430+TRK73@users.noreply.github.com>
2023-05-26 15:39:35 +0300

    Add files via upload

--- AI.py
+++ AI.py
@@ -1,36 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-import requests
 import random
-from langdetect import detect
-import re
-from googlesearch import search
+import wikipediaapi
 from bs4 import BeautifulSoup
+import requests
+import json
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__, static_folder='static')
+CORS(app, origins='*')
 
-def is_ad_link(link):
-    ad_patterns = [
-        "googleadservices.com",
-        "doubleclick.net",
-        "adservice.google.com",
-        "adclick",
-        "adurl",
-        "adclick.",
-        "adserver",
-        "ad-",
-        "/ads/",
-        "/ad/",
-        "/ads?",
-        "utm_medium=cpc",
-        "utm_campaign=",
-        "utm_source=google",
-    ]
-    for pattern in ad_patterns:
-        if pattern in link:
-            return True
-    return False
+
 welcome_answers = [
     "Hi, How are you today?",
     "Hello, How are you today?",
@@ -62,13 +41,31 @@ solve_answers = [
     "I'm sorry about that. Can I help you with something?",
     "No problem, everybody can feel sad."
 ]
+what_is_you_name_answers = [
+    "My name is (Chatbot AI).",
+    "I am an AI i don't have a special name but you can call me (Chatbot AI).",
+]
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.get_json()
-    message = data['message']
-    reply = process_message(message)
-    return jsonify({'reply': reply})
+@app.route('/')
+def index():
+    return render_template('aichatbot.html')
+
+
+def search_on_google(query):
+    search_results = []
+    url = f"https://www.google.com/search?q={query}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extract search results
+    search_results = soup.find_all("div", class_="tF2Cxc")
+
+    return search_results
+
 
 def process_message(message):
     message = message.lower()
@@ -78,13 +75,13 @@ def process_message(message):
     elif message == "fine, thanks" or message == "fine" or message == "thanks" or message == "thank you" or message == "happy" or message == "very happy" or message == "great" or message == "good":
         return random.choice(happy_answers)
     elif message == "what's your name" or message == "what is your name" or message == "how to change your name":
-        return random.choice(confusion_answers)
-    
+        return random.choice(what_is_you_name_answers)
+
     elif message == "i feel bad" or message == "i feel sad" or message == "sad" or message == "bad" or message == "very bad" or message == "very sad":
         return random.choice(sad_answers)
     elif message == "i got low marks" or message == "i got bad marks" or message == "my friend left me" or message == "my friends left me" or message == "i don't have friends" or message == "everyone hates me" or message == "nobody loves me":
         return random.choice(solve_answers)
-    
+
     if message.startswith("calculate"):
         expression = message[10:]  # Extract the expression to be calculated
         try:
@@ -94,34 +91,61 @@ def process_message(message):
             return "Oops! Something went wrong. Please check your expression."
 
     else:
-        # Perform a Google search based on the user's input
-        query = message
-        search_results = search(query, num_results=5, lang='en')
+        # Search Wikipedia for the user's input
+        wiki_wiki = wikipediaapi.Wikipedia('en')
+        page = wiki_wiki.page(message)
 
-        # Filter out ads and unwanted links from the search results
-        filtered_results = []
-        for result in search_results:
-            if not is_ad_link(result):
-                filtered_results.append(result)
-
-        if len(filtered_results) > 0:
-            answers = []
-            for result in filtered_results:
-                try:
-                    response = requests.get(result)
-                    soup = BeautifulSoup(response.text, "html.parser")
-                    answer = soup.get_text().strip()
-                    answers.append(answer)
-                except Exception:
-                    pass
-
-            if len(answers) > 0:
-                return ' '.join(answers)
-            else:
-                return "Sorry, I couldn't find any relevant information."
+        if page.exists():
+            return page.summary
 
         else:
-            return "Sorry, I couldn't find any relevant information."
+            # Search on Google using the user's input
+            search_results = search_on_google(message)
+
+            if search_results:
+                # Extract relevant information from the search results using BeautifulSoup
+                info_list = []
+                for result in search_results:
+                    title = result.find('h3').get_text()
+                    snippet_element = result.find('span', class_='aCOpRe')
+
+                    if snippet_element:
+                        snippet = snippet_element.get_text()
+                    else:
+                        snippet = ""
+
+                    info_list.append({
+                        'title': title,
+                        'snippet': snippet
+                    })
+
+                # Generate response with larger snippets
+                response_text = ""
+                for info in info_list:
+                    title = info['title']
+                    snippet = info['snippet']
+
+                    # Adjust the number of characters to display from the snippet
+                    max_characters = 2500
+                    if len(snippet) > max_characters:
+                        snippet = snippet[:max_characters] + '...'
+
+                    response_text += f"{title}\n{snippet}\n\n"
+
+                return response_text
+
+            else:
+                return random.choice(confusion_answers)
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_message = data['message']
+    ai_message = process_message(user_message)
+    response = {'message': ai_message}
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run()
